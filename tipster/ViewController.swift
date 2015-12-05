22b2b2017ad2985d6a9b54d129f79bd313bdd79c
Sarn Wattanasri <sarn@sarns-macbook-pro.local>
2015-12-04 21:42:03 -0800

    Initial commit

--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegmentedControl: UISegmentedControl!
+    
+    let tipPercentages = [0.18, 0.2, 0.22]
+    
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercent = tipPercentages[tipSegmentedControl.selectedSegmentIndex]
+        let billAmount = (billTextField.text! as NSString).doubleValue
+        let tip = billAmount * tipPercent
+        let total = billAmount + tip
+        tipLabel.text = formatCurrency(tip)
+        totalLabel.text = formatCurrency(total)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //calculate the total amount given a bill amount and tip percentage
+    
+    
+    //format the currency amount
+    func formatCurrency(amount: Double) -> String {
+        return String(format: "$%.2f", amount)
     }
 
 
+    @IBAction func onTapped(sender: AnyObject) {
+        //dismiss the keyboard
+        view.endEditing(true)
+    }
 }
 
