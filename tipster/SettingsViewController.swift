faf142b3ef5c77a153fdf52c0ad1a3510c5a2dae
Sarn Wattanasri <sarn@sarns-macbook-pro.local>
2015-12-05 14:08:52 -0800

    Embeded in Navigation Controller and add Settings bar button

---
+++ tipster/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  tipster
+//
+//  Created by Sarn Wattanasri on 12/5/15.
+//  Copyright © 2015 Sarn. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("settings view loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
